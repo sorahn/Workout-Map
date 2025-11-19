0d33b5a4ccbd84a8bc435ce58964e6ddb19dcd87
Daryl Roberts <daryl@metart.com>
2025-11-18 18:56:20 -1000

    Initial Commit

---
+++ Workout Map/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Workout Map
+//
+//  Created by Daryl Roberts on 11/18/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Workout Map/Workout_MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  Workout_MapApp.swift
+//  Workout Map
+//
+//  Created by Daryl Roberts on 11/18/25.
+//
+
+import SwiftUI
+
+@main
+struct Workout_MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
